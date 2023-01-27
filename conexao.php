575c0599ecfd83c9114afbeb5b0e82cbb6845d33
Leonardo Aragão <leonardo.lca@isgh.org.br>
2023-01-27 16:00:53 -0300

    conexao desenv e prod

--- conexao.php
+++ conexao.php
@@ -1,9 +1,16 @@
 <?php
 
+// DESENVOLVIMENTO
 $hostname = 'localhost';
 $database = 'test'; 
 $db_user = 'root'; 
 $db_pass = ''; 
+
+// PRODUÇÃO
+// $hostname = 'localhost';
+// $database = 'u882876507_to_do_list_php'; 
+// $db_user = 'u882876507_root'; 
+// $db_pass = 'Senha@teste1'; 
 
 $conn = mysqli_connect("$hostname" , "$db_user" , "$db_pass", "$database");;
 
