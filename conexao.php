922757db0cc2553d1c402b4324a5456535368f33
Leonardo Aragão <65857778+Lewoaragao@users.noreply.github.com>
2023-02-05 17:41:09 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -12,7 +12,7 @@ $db_pass = '';
 // $db_user = 'u882876507_root'; 
 // $db_pass = 'Senha@teste1'; 
 
-$conn = mysqli_connect("$hostname" , "$db_user" , "$db_pass", "$database");;
+$conn = mysqli_connect("$hostname" , "$db_user" , "$db_pass", "$database");
 
 if (!$conn) {
     die("Não foi possível se conectar ao banco de dados.");
